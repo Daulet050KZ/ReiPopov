15bc47a788d7dd3f1bc52cffc9212b16600445d0
Daulet050KZ <124065762+Daulet050KZ@users.noreply.github.com>
2024-06-28 20:58:57 +0300

    Улучшения 2.0

--- js/script.js
+++ js/script.js
@@ -1,47 +1,47 @@
 const intro = document.querySelector('.intro')
 const header = document.querySelector('#header')
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-	var currentScrollPos = window.pageYOffset;
-	if(380 < window.pageYOffset){
-		header.style.backgroundColor = '#222222'
-	}else{
-		header.style.backgroundColor = ''
-	}
-	if (1700 < window.pageYOffset) {
-		setTimeout(() => {
-			diagramm_photoshop.style.height = '90%'
-		}, 100);
-		setTimeout(() => {
-			diagramm_illustrator.style.height = '80%'
-		}, 100);
-		setTimeout(() => {
-			diagramm_corelDraw.style.height = '80%'
-		}, 100);
-		setTimeout(() => {
-			diagramm_figma.style.height = '70%'
-		}, 100);
-	}else {
-		setTimeout(() => {
-			diagramm_photoshop.style.height = '0%'
-		}, 100);
-		setTimeout(() => {
-			diagramm_illustrator.style.height = '0%'
-		}, 100);
-		setTimeout(() => {
-			diagramm_figma.style.height = '0%'
-		}, 100);
-		setTimeout(() => {
-			diagramm_corelDraw.style.height = '0%'
-		}, 100);
-	}
-}
+// const diagramm_photoshop = document.querySelector('.section__diagramm.Photoshop').querySelector('.diagramm')
+// const diagramm_illustrator = document.querySelector('.section__diagramm.Illustrator').querySelector('.diagramm')
+// const diagramm_figma = document.querySelector('.section__diagramm.Figma').querySelector('.diagramm')
+// const diagramm_corelDraw = document.querySelector('.section__diagramm.CorelDraw').querySelector('.diagramm')
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// 	var currentScrollPos = window.pageYOffset;
+// 	if(380 < window.pageYOffset){
+// 		header.style.backgroundColor = '#222222'
+// 	}else{
+// 		header.style.backgroundColor = ''
+// 	}
+// 	if (1700 < window.pageYOffset) {
+// 		setTimeout(() => {
+// 			diagramm_photoshop.style.height = '90%'
+// 		}, 100);
+// 		setTimeout(() => {
+// 			diagramm_illustrator.style.height = '80%'
+// 		}, 100);
+// 		setTimeout(() => {
+// 			diagramm_corelDraw.style.height = '80%'
+// 		}, 100);
+// 		setTimeout(() => {
+// 			diagramm_figma.style.height = '70%'
+// 		}, 100);
+// 	}else {
+// 		setTimeout(() => {
+// 			diagramm_photoshop.style.height = '0%'
+// 		}, 100);
+// 		setTimeout(() => {
+// 			diagramm_illustrator.style.height = '0%'
+// 		}, 100);
+// 		setTimeout(() => {
+// 			diagramm_figma.style.height = '0%'
+// 		}, 100);
+// 		setTimeout(() => {
+// 			diagramm_corelDraw.style.height = '0%'
+// 		}, 100);
+// 	}
+// }
 
-const diagramm_photoshop = document.querySelector('.section__diagramm.Photoshop').querySelector('.diagramm')
-const diagramm_illustrator = document.querySelector('.section__diagramm.Illustrator').querySelector('.diagramm')
-const diagramm_figma = document.querySelector('.section__diagramm.Figma').querySelector('.diagramm')
-const diagramm_corelDraw = document.querySelector('.section__diagramm.CorelDraw').querySelector('.diagramm')
 
 document.querySelector('.stage__0').addEventListener('click', function(){
 	document.querySelector('.intro').scrollIntoView({ behavior: 'smooth' });
@@ -92,47 +92,128 @@ document.querySelector('#certif__label-3').onclick = function(){
 
 function handleResize() {
     if (window.matchMedia('(max-width: 1023px)').matches) {
-    	document.querySelector('.review_1').style.marginLeft = '40px';
+		const diagramm_photoshop = document.querySelector('.section__diagramm.Photoshop').querySelector('.diagramm')
+		const diagramm_illustrator = document.querySelector('.section__diagramm.Illustrator').querySelector('.diagramm')
+		const diagramm_figma = document.querySelector('.section__diagramm.Figma').querySelector('.diagramm')
+		const diagramm_corelDraw = document.querySelector('.section__diagramm.CorelDraw').querySelector('.diagramm')
+		var prevScrollpos = window.pageYOffset;
+		window.onscroll = function() {
+			var currentScrollPos = window.pageYOffset;
+			if(380 < window.pageYOffset){
+				header.style.backgroundColor = '#222222'
+			}else{
+				header.style.backgroundColor = ''
+			}
+			if (3350 < window.pageYOffset) {
+				setTimeout(() => {
+					diagramm_photoshop.style.width = '90%'
+				}, 100);
+				setTimeout(() => {
+					diagramm_illustrator.style.width = '80%'
+				}, 100);
+				setTimeout(() => {
+					diagramm_corelDraw.style.width = '80%'
+				}, 100);
+				setTimeout(() => {
+					diagramm_figma.style.width = '70%'
+				}, 100);
+			}else {
+				setTimeout(() => {
+					diagramm_photoshop.style.width = '0%'
+				}, 100);
+				setTimeout(() => {
+					diagramm_illustrator.style.width = '0%'
+				}, 100);
+				setTimeout(() => {
+					diagramm_figma.style.width = '0%'
+				}, 100);
+				setTimeout(() => {
+					diagramm_corelDraw.style.width = '0%'
+				}, 100);
+			}
+		}
+
+    	document.querySelector('.review_1').style.marginLeft = '-10px';
     	document.querySelector('#reviews__label-left').onclick = function() {
-    		if(document.querySelector('.review_1').style.marginLeft === '-2920px'){
-    			document.querySelector('.review_1').style.marginLeft = '-2550px';
-    		}else if(document.querySelector('.review_1').style.marginLeft === '-2550px'){
-    			document.querySelector('.review_1').style.marginLeft = '-2180px';
-    		}else if(document.querySelector('.review_1').style.marginLeft === '-2180px'){
-    			document.querySelector('.review_1').style.marginLeft = '-1810px';
-    		}else if(document.querySelector('.review_1').style.marginLeft === '-1810px'){
-    			document.querySelector('.review_1').style.marginLeft = '-1440px';
-    		}else if(document.querySelector('.review_1').style.marginLeft === '-1440px'){
-    			document.querySelector('.review_1').style.marginLeft = '-1070px';
-    		}else if(document.querySelector('.review_1').style.marginLeft === '-1070px'){
-    			document.querySelector('.review_1').style.marginLeft = '-700px';
-    		}else if(document.querySelector('.review_1').style.marginLeft === '-700px'){
-    			document.querySelector('.review_1').style.marginLeft = '-330px';
-    		}else if(document.querySelector('.review_1').style.marginLeft === '-330px'){
-    			document.querySelector('.review_1').style.marginLeft = '40px';
+    		if(document.querySelector('.review_1').style.marginLeft === '-2970px'){
+    			document.querySelector('.review_1').style.marginLeft = '-2600px';
+    		}else if(document.querySelector('.review_1').style.marginLeft === '-2600px'){
+    			document.querySelector('.review_1').style.marginLeft = '-2230px';
+    		}else if(document.querySelector('.review_1').style.marginLeft === '-2230px'){
+    			document.querySelector('.review_1').style.marginLeft = '-1860px';
+    		}else if(document.querySelector('.review_1').style.marginLeft === '-1860px'){
+    			document.querySelector('.review_1').style.marginLeft = '-1490px';
+    		}else if(document.querySelector('.review_1').style.marginLeft === '-1490px'){
+    			document.querySelector('.review_1').style.marginLeft = '-1120px';
+    		}else if(document.querySelector('.review_1').style.marginLeft === '-1120px'){
+    			document.querySelector('.review_1').style.marginLeft = '-750px';
+    		}else if(document.querySelector('.review_1').style.marginLeft === '-750px'){
+    			document.querySelector('.review_1').style.marginLeft = '-380px';
+    		}else if(document.querySelector('.review_1').style.marginLeft === '-380px'){
+    			document.querySelector('.review_1').style.marginLeft = '-10px';
     		}
 		};
     	document.querySelector('#reviews__label-right').onclick = function() {
-    		if(document.querySelector('.review_1').style.marginLeft === '-2550px'){
-    			document.querySelector('.review_1').style.marginLeft = '-2920px';
-    		}else if(document.querySelector('.review_1').style.marginLeft === '-2180px'){
-    			document.querySelector('.review_1').style.marginLeft = '-2550px';
-    		}else if(document.querySelector('.review_1').style.marginLeft === '-1810px'){
-    			document.querySelector('.review_1').style.marginLeft = '-2180px';
-    		}else if(document.querySelector('.review_1').style.marginLeft === '-1440px'){
-    			document.querySelector('.review_1').style.marginLeft = '-1810px';
-    		}else if(document.querySelector('.review_1').style.marginLeft === '-1070px'){
-    			document.querySelector('.review_1').style.marginLeft = '-1440px';
-    		}else if(document.querySelector('.review_1').style.marginLeft === '-700px'){
-    			document.querySelector('.review_1').style.marginLeft = '-1070px';
-    		}else if(document.querySelector('.review_1').style.marginLeft === '-330px'){
-    			document.querySelector('.review_1').style.marginLeft = '-700px';
-    		}else if(document.querySelector('.review_1').style.marginLeft === '40px'){
-    			document.querySelector('.review_1').style.marginLeft = '-330px';
+    		if(document.querySelector('.review_1').style.marginLeft === '-2600px'){
+    			document.querySelector('.review_1').style.marginLeft = '-2970px';
+    		}else if(document.querySelector('.review_1').style.marginLeft === '-2230px'){
+    			document.querySelector('.review_1').style.marginLeft = '-2600px';
+    		}else if(document.querySelector('.review_1').style.marginLeft === '-1860px'){
+    			document.querySelector('.review_1').style.marginLeft = '-2230px';
+    		}else if(document.querySelector('.review_1').style.marginLeft === '-1490px'){
+    			document.querySelector('.review_1').style.marginLeft = '-1860px';
+    		}else if(document.querySelector('.review_1').style.marginLeft === '-1120px'){
+    			document.querySelector('.review_1').style.marginLeft = '-1490px';
+    		}else if(document.querySelector('.review_1').style.marginLeft === '-750px'){
+    			document.querySelector('.review_1').style.marginLeft = '-1120px';
+    		}else if(document.querySelector('.review_1').style.marginLeft === '-380px'){
+    			document.querySelector('.review_1').style.marginLeft = '-750px';
+    		}else if(document.querySelector('.review_1').style.marginLeft === '-10px'){
+    			document.querySelector('.review_1').style.marginLeft = '-380px';
     		}
 		};
 
     } else {
+		const diagramm_photoshop = document.querySelector('.section__diagramm.Photoshop').querySelector('.diagramm')
+		const diagramm_illustrator = document.querySelector('.section__diagramm.Illustrator').querySelector('.diagramm')
+		const diagramm_figma = document.querySelector('.section__diagramm.Figma').querySelector('.diagramm')
+		const diagramm_corelDraw = document.querySelector('.section__diagramm.CorelDraw').querySelector('.diagramm')
+		var prevScrollpos = window.pageYOffset;
+		window.onscroll = function() {
+			var currentScrollPos = window.pageYOffset;
+			if(380 < window.pageYOffset){
+				header.style.backgroundColor = '#222222'
+			}else{
+				header.style.backgroundColor = ''
+			}
+			if (1700 < window.pageYOffset) {
+				setTimeout(() => {
+					diagramm_photoshop.style.height = '90%'
+				}, 100);
+				setTimeout(() => {
+					diagramm_illustrator.style.height = '80%'
+				}, 100);
+				setTimeout(() => {
+					diagramm_corelDraw.style.height = '80%'
+				}, 100);
+				setTimeout(() => {
+					diagramm_figma.style.height = '70%'
+				}, 100);
+			}else {
+				setTimeout(() => {
+					diagramm_photoshop.style.height = '0%'
+				}, 100);
+				setTimeout(() => {
+					diagramm_illustrator.style.height = '0%'
+				}, 100);
+				setTimeout(() => {
+					diagramm_figma.style.height = '0%'
+				}, 100);
+				setTimeout(() => {
+					diagramm_corelDraw.style.height = '0%'
+				}, 100);
+			}
+		}
 		document.getElementById('reviews__label-1').onclick = function(){
 			document.querySelector('.review_1').style.transform = 'translateY(calc(0%))'
 			document.querySelector('.review_2').style.transform = 'translateY(calc(0%))'
